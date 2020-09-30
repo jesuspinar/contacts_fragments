f3af13607cd8885e9e78669bcce54952dcf2b9f6
Germán Gascón <ggascon@gmail.com>
2020-09-30 16:39:11 +0200

    Updated to androidx and API 29

--- app/src/main/java/com/germangascon/fragments/DetalleActivity.java
+++ app/src/main/java/com/germangascon/fragments/DetalleActivity.java
@@ -1,7 +1,7 @@
 package com.germangascon.fragments;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DetalleActivity extends AppCompatActivity {
 
